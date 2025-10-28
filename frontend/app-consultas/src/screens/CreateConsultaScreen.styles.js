e096a663f5adbd51c10004f94c4e808b5a4d223a
Felipe <felypebueno2004@gmail.com>
2025-10-27 21:42:46 -0300

    Update estilization.

--- frontend/app-consultas/src/screens/CreateConsultaScreen.styles.js
+++ frontend/app-consultas/src/screens/CreateConsultaScreen.styles.js
@@ -1,15 +1,24 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-  container: { flex: 1, padding: 20, backgroundColor: '#fff' },
-  label: { fontSize: 16, marginBottom: 5, color: '#333' },
-  input: {
-    height: 45,
-    borderColor: '#ccc',
-    borderWidth: 1,
-    marginBottom: 20,
-    paddingHorizontal: 10,
-    borderRadius: 8,
+  container: { 
+    flex: 1, 
+    padding: 20, 
+    backgroundColor: '#fff' 
+  },
+  label: { 
+    fontSize: 16, 
+    marginBottom: 5, 
+    color: '#333' 
+  },
+  input: { 
+    height: 45, 
+    borderColor: '#ccc', 
+    borderWidth: 1, 
+    marginBottom: 20, 
+    paddingHorizontal: 10, 
+    borderRadius: 8, 
     fontSize: 16,
+    backgroundColor: '#fff'  // Adicionado para garantir fundo branco
   },
 });
